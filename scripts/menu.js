b933a958418db2af5a15bfae9587f028b55039c1
Ragavanmsd <75945143+Ragavanmsd@users.noreply.github.com>
2021-02-06 18:06:58 +0530

    Initial commit

---
+++ scripts/menu.js
@@ -0,0 +1,61 @@
+
+
+const checkboxes= document.querySelectorAll('.inbox  input[type="checkbox"]');
+
+let lastChecked;
+
+function handleChange(e){
+
+    let inBetween=false;
+    if (e.shiftKey && this.checked){
+        checkboxes.forEach(checkbox=>{
+            console.log(checkbox);  
+            
+        if (checkbox === this || checkbox === lastChecked){
+            inBetween=!inBetween;
+            console.log('start checking')
+        }
+        if (inBetween){
+            checkbox.checked=true;
+        }
+        })
+    }
+    lastChecked=this;
+
+}
+
+checkboxes.forEach(checkbox => 
+    checkbox.addEventListener('click', handleChange));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
